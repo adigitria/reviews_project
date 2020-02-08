53b79e597a16660ed8131c37830b86ad4e65b93b
Artem Tsepkov <tzepart@gmail.com>
2020-02-08 15:34:56 +0300

    add container_runner.sh

---
+++ container_runner.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+url=$1
+countPage=$2
+countProject=$3
+startPage=1
+step=$(( countPage / countProject ))
+for (( i=1; i <= $countProject; i++ ))
+do
+  project_number=$(( i - 1 ))
+  cd ~/review_project_$project_number
+  if [ $i -eq $countProject ]
+  then
+  finalPage=$countPage
+  else
+  finalPage=$(( startPage + step - 1 ))
+  fi
+  sudo docker-compose run -d --rm app ./runner $url $startPage $finalPage
+  startPage=$(( finalPage + 1 ))
+done
