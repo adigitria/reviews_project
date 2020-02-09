3104f080e085e4043321653d450b120ef040d87a
adigitria <s.m.bogomaz@gmail.com>
2020-02-10 01:18:57 +0300

    upd container_runner.sh

--- container_runner.sh
+++ container_runner.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
+
 url=$1
 countPage=$2
-countProject=$3
+startProjectNumber=$(( $3 - 1 ))
+countProject=$4
+finalProjectNumber=$(( startProjectNumber + countProject))
 startPage=1
 step=$(( countPage / countProject ))
-for (( i=1; i <= $countProject; i++ ))
+for (( i=$startProjectNumber; i < $finalProjectNumber; i++ ))
 do
-  project_number=$(( i - 1 ))
+  project_number=$(( i ))
+  currentCountProjects=$(( i + 1 ))
   cd ~/reviews_project_$project_number
-  if [ $i -eq $countProject ]
+  if [ $currentCountProjects -eq $finalProjectNumber ]
   then
   finalPage=$countPage
   else
   finalPage=$(( startPage + step - 1 ))
   fi
+#  echo $project_number $url $startPage $finalPage
   sudo docker-compose run -d --rm app ./runner $url $startPage $finalPage
   startPage=$(( finalPage + 1 ))
 done
