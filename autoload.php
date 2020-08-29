a10d12d18a30ce834281b5471167c0d6e3dda7a0
tzepart <tzepart@gmail.com>
2020-08-30 01:50:41 +0300

    add composer

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-spl_autoload_register(
-    function ($class) {
-        $class = str_replace(['\\', 'ReviewParser/'], ['/', ''], $class);
-        include 'src/' . $class . '.php';
-    }
-);
